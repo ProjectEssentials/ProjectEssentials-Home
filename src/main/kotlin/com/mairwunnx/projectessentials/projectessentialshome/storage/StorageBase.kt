c5a0f928711ced59edd2f6ccf6b20bd29b51de0a
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-01-22 17:44:46 +0300

    Usings jsonInstance from core module. createConfigDirs method from StorageBase removed. UseExperimental annotation for StorageBase class removed. Redundant logger messages removed from StorageBase.kt.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/projectessentialshome/storage/StorageBase.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/projectessentialshome/storage/StorageBase.kt
@@ -1,16 +1,13 @@
 package com.mairwunnx.projectessentials.projectessentialshome.storage
 
+import com.mairwunnx.projectessentials.core.helpers.MOD_CONFIG_FOLDER
+import com.mairwunnx.projectessentials.core.helpers.jsonInstance
 import com.mairwunnx.projectessentials.projectessentialshome.models.HomeModel
-import com.mairwunnx.projectessentialscore.helpers.MOD_CONFIG_FOLDER
-import kotlinx.serialization.UnstableDefault
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 import org.apache.logging.log4j.LogManager
 import java.io.File
 import java.io.FileNotFoundException
 import kotlin.system.measureTimeMillis
 
-@UseExperimental(UnstableDefault::class)
 object StorageBase {
     private val logger = LogManager.getLogger()
     private val userHomeData = hashMapOf<String, HomeModel>()
@@ -32,9 +29,9 @@ object StorageBase {
     }
 
     fun loadUserData() {
-        logger.info("    - loading user home data configuration ...")
+        logger.info("Loading user home data configuration")
 
-        createConfigDirs(userDataFolder)
+        File(userDataFolder).mkdirs()
         val users = File(userDataFolder).list()?.filter {
             if (File(it).isFile) return@filter false
             return@filter true
@@ -42,16 +39,17 @@ object StorageBase {
 
         val elapsedTime = measureTimeMillis {
             users?.forEach {
-                logger.debug("        - processing $it user home data ...")
                 val userId = it
                 try {
                     val userDataRaw = File(
                         userDataFolder + File.separator + it + File.separator + "home.json"
                     ).readText()
-                    val userHomeClass = Json.parse(HomeModel.serializer(), userDataRaw)
+                    val userHomeClass = jsonInstance.parse(
+                        HomeModel.serializer(), userDataRaw
+                    )
                     userHomeData[userId] = userHomeClass
                 } catch (_: FileNotFoundException) {
-                    logger.info("        - loading home data for $it skipped! not found!")
+                    logger.info("Loading home data for $it skipped! not found!")
                 }
             }
         }
@@ -59,39 +57,20 @@ object StorageBase {
     }
 
     fun saveUserData() {
-        createConfigDirs(userDataFolder)
+        File(userDataFolder).mkdirs()
         userHomeData.keys.forEach {
-            logger.debug("        - processing $it user home data ...")
-
             val userId = it
             val userDataClass = userHomeData[userId]!!
             val dataFolder = userDataFolder + File.separator + userId
             val dataPath = dataFolder + File.separator + "home.json"
 
-            createConfigDirs(dataFolder)
-            logger.debug("        - setup json configuration for parsing ...")
-            val json = Json(
-                JsonConfiguration(
-                    encodeDefaults = true,
-                    strictMode = true,
-                    unquoted = false,
-                    allowStructuredMapKeys = true,
-                    prettyPrint = true,
-                    useArrayPolymorphism = false
-                )
-            )
-            val userDataRaw = json.stringify(HomeModel.serializer(), userDataClass)
+            File(dataFolder).mkdirs()
+            val userDataRaw = jsonInstance.stringify(HomeModel.serializer(), userDataClass)
             try {
                 File(dataPath).writeText(userDataRaw)
             } catch (ex: SecurityException) {
                 logger.error("An error occurred while saving home configuration", ex)
             }
         }
-    }
-
-    private fun createConfigDirs(path: String) {
-        logger.info("        - creating config directory for user home data ($path)")
-        val configDirectory = File(path)
-        if (!configDirectory.exists()) configDirectory.mkdirs()
     }
 }
