146161cb9187b0f50f986452c3121f51f38fee40
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-02 19:40:50 +0300

    Sending providers and localization added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/home/ModuleObject.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/home/ModuleObject.kt
@@ -1,6 +1,16 @@
 package com.mairwunnx.projectessentials.home
 
+import com.mairwunnx.projectessentials.core.api.v1.IMCLocalizationMessage
+import com.mairwunnx.projectessentials.core.api.v1.IMCProvidersMessage
+import com.mairwunnx.projectessentials.core.api.v1.events.ModuleEventAPI.subscribeOn
+import com.mairwunnx.projectessentials.core.api.v1.events.forge.ForgeEventType
+import com.mairwunnx.projectessentials.core.api.v1.events.forge.InterModEnqueueEventData
 import com.mairwunnx.projectessentials.core.api.v1.module.IModule
+import com.mairwunnx.projectessentials.home.commands.DelHomeCommand
+import com.mairwunnx.projectessentials.home.commands.HomeCommand
+import com.mairwunnx.projectessentials.home.commands.SetHomeCommand
+import net.minecraftforge.common.MinecraftForge.EVENT_BUS
+import net.minecraftforge.fml.InterModComms
 import net.minecraftforge.fml.common.Mod
 
 @Mod("project_essentials_home")
@@ -9,4 +19,43 @@ class ModuleObject : IModule {
     override val version = this::class.java.`package`.implementationVersion!!
     override val loadIndex = 4
     override fun init() = Unit
+
+    init {
+        EVENT_BUS.register(this)
+        subscribeOn<InterModEnqueueEventData>(
+            ForgeEventType.EnqueueIMCEvent
+        ) {
+            sendLocalizationRequest()
+            sendProvidersRequest()
+        }
+    }
+
+    private fun sendLocalizationRequest() {
+        InterModComms.sendTo(
+            "project_essentials_core",
+            IMCLocalizationMessage
+        ) {
+            fun() = mutableListOf(
+                "/assets/projectessentialshome/lang/en_us.json",
+                "/assets/projectessentialshome/lang/ru_ru.json",
+                "/assets/projectessentialshome/lang/de_de.json",
+                "/assets/projectessentialshome/lang/zh_cn.json"
+            )
+        }
+    }
+
+    private fun sendProvidersRequest() {
+        InterModComms.sendTo(
+            "project_essentials_core",
+            IMCProvidersMessage
+        ) {
+            fun() = listOf(
+                SetHomeCommand::class.java,
+                HomeCommand::class.java,
+                DelHomeCommand::class.java,
+                HomeConfiguration::class.java,
+                HomeSettingsConfiguration::class.java
+            )
+        }
+    }
 }
