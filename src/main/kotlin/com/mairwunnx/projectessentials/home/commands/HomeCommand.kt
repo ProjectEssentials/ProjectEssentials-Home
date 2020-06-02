79a15cae7d240d1a9869ab54eb0a3f8af8aad5fb
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-02 21:57:07 +0300

    Home command re-written.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/home/commands/HomeCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/home/commands/HomeCommand.kt
@@ -1,98 +1,45 @@
 package com.mairwunnx.projectessentials.home.commands
 
-import com.mairwunnx.projectessentials.cooldown.essentials.CommandsAliases
-import com.mairwunnx.projectessentials.core.backlocation.BackLocationProvider
-import com.mairwunnx.projectessentials.core.extensions.isPlayerSender
-import com.mairwunnx.projectessentials.core.helpers.throwOnlyPlayerCan
-import com.mairwunnx.projectessentials.core.helpers.throwPermissionLevel
-import com.mairwunnx.projectessentials.home.EntryPoint
-import com.mairwunnx.projectessentials.home.EntryPoint.Companion.hasPermission
-import com.mairwunnx.projectessentials.home.HomeAPI
-import com.mairwunnx.projectessentials.home.models.HomeModel
-import com.mairwunnx.projectessentials.home.sendMessage
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.arguments.StringArgumentType
-import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
+import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
+import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
+import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
+import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
+import com.mairwunnx.projectessentials.home.helpers.validateAndExecute
+import com.mairwunnx.projectessentials.home.homeConfiguration
+import com.mairwunnx.projectessentials.home.teleportToHome
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
-import net.minecraft.command.Commands
-import net.minecraft.entity.player.ServerPlayerEntity
-import net.minecraft.world.dimension.DimensionType
-import org.apache.logging.log4j.LogManager
 
-internal object HomeCommand {
-    private val aliases = arrayOf("home", "ehome")
-    private val logger = LogManager.getLogger()
-
-    fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        logger.info("Register \"/home\" command")
-        applyCommandAliases()
-
-        aliases.forEach { command ->
-            dispatcher.register(
-                literal<CommandSource>(command).executes {
-                    return@executes execute(it)
-                }.then(
-                    Commands.argument(
-                        "home name", StringArgumentType.string()
-                    ).executes {
-                        return@executes execute(it)
-                    }
-                )
-            )
-        }
-    }
-
-    private fun applyCommandAliases() {
-        if (!EntryPoint.cooldownsInstalled) return
-        CommandsAliases.aliases["home"] = aliases.toMutableList()
-    }
-
-    private fun execute(c: CommandContext<CommandSource>): Int {
-        if (c.isPlayerSender()) {
-            val player = c.source.asPlayer()
-            if (hasPermission(player, "ess.home")) {
-                val homeName: String = try {
-                    StringArgumentType.getString(c, "home name")
-                } catch (_: IllegalArgumentException) {
-                    "home"
-                }
-
-                HomeAPI.take(player, homeName)?.let {
-                    moveToHome(player, it)
-                    return 0
-                }
-
-                sendMessage(c.source, "not_found", homeName)
-                logger.info("Player ${player.name.string} try teleport to not exist home $homeName")
-            } else {
-                sendMessage(c.source, "restricted")
-                throwPermissionLevel(player.name.string, "home")
-            }
-        } else {
-            throwOnlyPlayerCan("home")
-        }
-        return 0
-    }
-
-    private fun moveToHome(player: ServerPlayerEntity, home: HomeModel.Home) {
-        val xPos = home.xPos.toDouble()
-        val yPos = home.yPos.toDouble()
-        val zPos = home.zPos.toDouble()
-        val yaw = home.yaw
-        val pitch = home.pitch
-        val dimId = home.worldId
-        val clientWorld = home.clientWorld
-        val targetWorld = player.server.getWorld(
-            DimensionType.getById(dimId) ?: DimensionType.OVERWORLD
+object HomeCommand : CommandBase(homeLiteral) {
+    override val name = "home"
+    override fun process(context: CommandContext<CommandSource>) = 0.also {
+        fun out(status: String, vararg args: String) = MessagingAPI.sendMessage(
+            context.getPlayer()!!, "${MESSAGE_MODULE_PREFIX}home.$status", args = *args
         )
-        if (player.world.worldInfo.worldName == clientWorld) {
-            BackLocationProvider.commit(player)
-            player.teleport(targetWorld, xPos, yPos, zPos, yaw, pitch)
-            sendMessage(player.commandSource, "success", home.home)
-        } else {
-            sendMessage(player.commandSource, "not_found", home.home)
-            logger.info("Player ${player.name.string} try teleport to not exist home ${home.home}")
+
+        validateAndExecute(context, "ess.home.teleport", 0) { isServer ->
+            if (isServer) {
+                ServerMessagingAPI.throwOnlyPlayerCan()
+            } else {
+                val player = context.getPlayer()!!
+                val name = if (CommandAPI.getStringExisting(context, "home")) {
+                    CommandAPI.getString(context, "home")
+                } else "home"
+
+                homeConfiguration.users.asSequence().find {
+                    it.name == player.name.string || it.uuid == player.uniqueID.toString()
+                }?.let { user ->
+                    user.homes.asSequence().find {
+                        it.home == name
+                    }?.let {
+                        teleportToHome(player, it).also {
+                            out("success").also { super.process(context) }
+                        }
+                    } ?: run { out("not_found") }
+                } ?: run { out("not_found") }
+            }
         }
     }
 }
