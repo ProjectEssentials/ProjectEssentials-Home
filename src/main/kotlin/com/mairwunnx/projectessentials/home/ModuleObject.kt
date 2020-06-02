6ec7a01443a7fa1b7371a9365e2fb74d1bbdab64
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-02 22:23:51 +0300

    Effects and sound on teleporting added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/home/ModuleObject.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/home/ModuleObject.kt
@@ -4,8 +4,6 @@ package com.mairwunnx.projectessentials.home
 
 import com.mairwunnx.projectessentials.core.api.v1.IMCLocalizationMessage
 import com.mairwunnx.projectessentials.core.api.v1.IMCProvidersMessage
-import com.mairwunnx.projectessentials.core.api.v1.commands.back.BackLocationAPI
-import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI
 import com.mairwunnx.projectessentials.core.api.v1.events.ModuleEventAPI.subscribeOn
 import com.mairwunnx.projectessentials.core.api.v1.events.forge.ForgeEventType
 import com.mairwunnx.projectessentials.core.api.v1.events.forge.InterModEnqueueEventData
@@ -14,29 +12,10 @@ import com.mairwunnx.projectessentials.home.commands.DelHomeCommand
 import com.mairwunnx.projectessentials.home.commands.HomeCommand
 import com.mairwunnx.projectessentials.home.commands.SetHomeCommand
 import com.mairwunnx.projectessentials.home.configurations.HomeConfiguration
-import com.mairwunnx.projectessentials.home.configurations.HomeConfigurationModel
 import com.mairwunnx.projectessentials.home.configurations.HomeSettingsConfiguration
-import net.minecraft.entity.player.ServerPlayerEntity
-import net.minecraft.world.dimension.DimensionType
 import net.minecraftforge.common.MinecraftForge.EVENT_BUS
 import net.minecraftforge.fml.InterModComms
 import net.minecraftforge.fml.common.Mod
-
-val homeConfiguration by lazy {
-    ConfigurationAPI.getConfigurationByName<HomeConfiguration>("home").take()
-}
-
-val homeSettingsConfiguration by lazy {
-    ConfigurationAPI.getConfigurationByName<HomeSettingsConfiguration>("home-settings").take()
-}
-
-fun teleportToHome(player: ServerPlayerEntity, home: HomeConfigurationModel.User.Home) {
-    BackLocationAPI.commit(player)
-    val world = player.server.getWorld(
-        DimensionType.getById(home.dimensionId) ?: DimensionType.OVERWORLD
-    )
-    player.teleport(world, home.xPos + 0.5, home.yPos + 0.5, home.zPos + 0.5, home.yaw, home.pitch)
-}
 
 @Mod("project_essentials_home")
 class ModuleObject : IModule {
