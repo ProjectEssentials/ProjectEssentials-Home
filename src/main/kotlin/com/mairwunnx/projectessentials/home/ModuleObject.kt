b97b461ad331997a62c127b670e124499470dc6e
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-08 18:47:53 +0300

    Providers and localizations resolution strategy changed.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/home/ModuleObject.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/home/ModuleObject.kt
@@ -2,16 +2,15 @@
 
 package com.mairwunnx.projectessentials.home
 
-import com.mairwunnx.projectessentials.core.api.v1.IMCLocalizationMessage
-import com.mairwunnx.projectessentials.core.api.v1.IMCProvidersMessage
-import com.mairwunnx.projectessentials.core.api.v1.events.ModuleEventAPI.subscribeOn
-import com.mairwunnx.projectessentials.core.api.v1.events.forge.ForgeEventType
-import com.mairwunnx.projectessentials.core.api.v1.events.forge.InterModEnqueueEventData
+import com.mairwunnx.projectessentials.core.api.v1.localization.Localization
+import com.mairwunnx.projectessentials.core.api.v1.localization.LocalizationAPI
 import com.mairwunnx.projectessentials.core.api.v1.module.IModule
+import com.mairwunnx.projectessentials.core.api.v1.providers.ProviderAPI
 import com.mairwunnx.projectessentials.home.commands.DelHomeCommand
 import com.mairwunnx.projectessentials.home.commands.HomeCommand
 import com.mairwunnx.projectessentials.home.commands.SetHomeCommand
 import com.mairwunnx.projectessentials.home.configurations.HomeConfiguration
+import com.mairwunnx.projectessentials.home.configurations.HomeConfigurationModel
 import com.mairwunnx.projectessentials.home.configurations.HomeSettingsConfiguration
 import com.mairwunnx.projectessentials.home.enums.HomeSelectStrategy.First
 import com.mairwunnx.projectessentials.home.enums.HomeSelectStrategy.Last
@@ -19,24 +18,67 @@ import net.minecraft.entity.player.ServerPlayerEntity
 import net.minecraftforge.common.MinecraftForge.EVENT_BUS
 import net.minecraftforge.event.entity.player.PlayerEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
-import net.minecraftforge.fml.InterModComms
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent
+import org.apache.logging.log4j.LogManager
 
 @Mod("project_essentials_home")
 class ModuleObject : IModule {
     override val name = this::class.java.`package`.implementationTitle.split(" ").last()
     override val version = this::class.java.`package`.implementationVersion!!
-    override val loadIndex = 4
+    override val loadIndex = 20
     override fun init() = Unit
 
     init {
         EVENT_BUS.register(this)
-        subscribeOn<InterModEnqueueEventData>(
-            ForgeEventType.EnqueueIMCEvent
-        ) {
-            sendLocalizationRequest()
-            sendProvidersRequest()
-        }
+        initProviders()
+        initLocalization()
+    }
+
+    private fun initProviders() {
+        listOf(
+            SetHomeCommand::class.java,
+            HomeCommand::class.java,
+            DelHomeCommand::class.java,
+            HomeConfiguration::class.java,
+            HomeSettingsConfiguration::class.java,
+            ModuleObject::class.java
+        ).forEach(ProviderAPI::addProvider)
+    }
+
+    private fun initLocalization() {
+        LocalizationAPI.apply(
+            Localization(
+                mutableListOf(
+                    "/assets/projectessentialshome/lang/en_us.json",
+                    "/assets/projectessentialshome/lang/ru_ru.json",
+                    "/assets/projectessentialshome/lang/de_de.json",
+                    "/assets/projectessentialshome/lang/zh_cn.json"
+                ), "core", this.javaClass
+            )
+        )
+//        LocalizationAPI.apply(this.javaClass) {
+//            mutableListOf(
+//                "/assets/projectessentialshome/lang/en_us.json",
+//                "/assets/projectessentialshome/lang/ru_ru.json",
+//                "/assets/projectessentialshome/lang/de_de.json",
+//                "/assets/projectessentialshome/lang/zh_cn.json"
+//            )
+//        }
+    }
+
+    @SubscribeEvent
+    fun onServerStart(event: FMLServerStartingEvent) {
+        LogManager.getLogger().info(homeConfiguration.users.count())
+        LogManager.getLogger().info("test")
+        LogManager.getLogger().info(
+            homeConfiguration.users.add(
+                HomeConfigurationModel.User(
+                    "test", "testuuid", mutableListOf()
+                )
+            )
+        )
+        LogManager.getLogger().info(homeConfiguration.users.count())
     }
 
     @SubscribeEvent
@@ -50,36 +92,6 @@ class ModuleObject : IModule {
                 Last -> teleportToHome(player, it.homes.last())
                 First -> teleportToHome(player, it.homes.first())
             }
-        }
-    }
-
-    private fun sendLocalizationRequest() {
-        InterModComms.sendTo(
-            "project_essentials_core",
-            IMCLocalizationMessage
-        ) {
-            fun() = mutableListOf(
-                "/assets/projectessentialshome/lang/en_us.json",
-                "/assets/projectessentialshome/lang/ru_ru.json",
-                "/assets/projectessentialshome/lang/de_de.json",
-                "/assets/projectessentialshome/lang/zh_cn.json"
-            )
-        }
-    }
-
-    private fun sendProvidersRequest() {
-        InterModComms.sendTo(
-            "project_essentials_core",
-            IMCProvidersMessage
-        ) {
-            fun() = listOf(
-                SetHomeCommand::class.java,
-                HomeCommand::class.java,
-                DelHomeCommand::class.java,
-                HomeConfiguration::class.java,
-                HomeSettingsConfiguration::class.java,
-                ModuleObject::class.java
-            )
         }
     }
 }
